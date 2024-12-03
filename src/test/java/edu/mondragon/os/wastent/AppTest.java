6435a423441e3c52ea777d42e64ec8e192ac4fe2
Amaia Bartolomé <amaiabart@gmail.com>
2024-12-03 10:18:28 +0100

    app test

--- src/test/java/edu/mondragon/os/wastent/AppTest.java
+++ src/test/java/edu/mondragon/os/wastent/AppTest.java
@@ -1,5 +1,13 @@
 package edu.mondragon.os.wastent;
 
+import org.junit.Test;
+
 public class AppTest {
-    
+
+    App app;
+
+    @Test
+    public void testMain() {
+        app.main(null);
+    }
 }
